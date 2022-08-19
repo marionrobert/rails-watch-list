747f34c1c565dbdabbfbc5e0b5262332ec3834d7
Marion Robert <mrobert148@gmail.com>
2022-08-19 16:47:05 +0200

    front styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
 puts "Cleaning database..."
 Movie.destroy_all
+List.destroy_all
 
 puts "Creating 4 movies..."
 
@@ -18,5 +21,20 @@ Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells
 puts "Create 1 movie"
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 puts "Create 1 movie"
+
+
+puts "Creating 2 lists..."
+
+file = URI.open("https://images8.alphacoders.com/110/1102284.jpg")
+list_hp = List.new(name: "Harry Potter")
+list_hp.photo.attach(io: file, filename: "hp.png", content_type: "image/png")
+list_hp.save
+puts "Create HP list"
+
+file = URI.open("https://images.pling.com/img/00/00/61/26/90/1551956/b46947476204206d2f280286e20d15f8528821ccbed13ca99deeb28d99a310694bce.jpg")
+list_hp = List.new(name: "Super Heroes")
+list_hp.photo.attach(io: file, filename: "avengers.png", content_type: "image/png")
+list_hp.save
+puts "Create Avenger list"
 
 puts 'Finished!'
