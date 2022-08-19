08e7eb58659c0cfa6fa9c3ce9b719dcaed0e75fb
Marion Robert <mrobert148@gmail.com>
2022-08-19 11:29:23 +0200

    deploying on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "lists#index"
+  resources :lists, only: [:show, :new, :create] do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
+
+  resources :bookmarks, only: [:destroy]
+
 end
